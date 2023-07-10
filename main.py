6934c4c92980f3b9966067f7ce6f9708a1bd3a13
Roman <roman1972@gmail.com>
2023-07-10 23:01:39 +0300

    Rework almost everything

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-# Author: Roman V. M.
-# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+# Copyright (C) 2023, Roman V. M.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 Example video plugin that is compatible with Kodi 20.x "Nexus" and above
 """
 import sys
+from pathlib import Path
 from urllib.parse import urlencode, parse_qsl
 
 import xbmcgui
 import xbmcplugin
+from xbmcaddon import Addon
+from xbmcvfs import translatePath
 
 # Get the plugin url in plugin:// notation.
-_URL = sys.argv[0]
-# Get the plugin handle as an integer number.
-_HANDLE = int(sys.argv[1])
+URL = sys.argv[0]
+# Get a plugin handle as an integer number.
+HANDLE = int(sys.argv[1])
+# Get addon base path
+ADDON_PATH = Path(translatePath(Addon().getAddonInfo('path')))
+ICONS_DIR = ADDON_PATH / 'resources' / 'images' / 'icons'
+FANART_DIR = ADDON_PATH / 'resources' / 'images' / 'fanart'
 
 # Public domain movies are from https://publicdomainmovie.net
 # Here we use a hardcoded list of movies simply for demonstrating purposes
@@ -21,7 +40,8 @@ _HANDLE = int(sys.argv[1])
 VIDEOS = [
     {
         'genre': 'Drama',
-        'icon': '',
+        'icon': str(ICONS_DIR / 'Drama.png'),
+        'fanart': str(FANART_DIR / 'Drama.jpg'),
         'movies': [
             {
                 'title': 'The Stranger',
@@ -56,7 +76,8 @@ VIDEOS = [
     },
     {
         'genre': 'Horror',
-        'icon': '',
+        'icon': str(ICONS_DIR / 'Horror.png'),
+        'fanart': str(FANART_DIR / 'Horror.jpg'),
         'movies': [
             {
                 'title': 'House on Haunted Hill',
@@ -82,7 +103,7 @@ def get_url(**kwargs):
     :return: plugin call URL
     :rtype: str
     """
-    return '{}?{}'.format(_URL, urlencode(kwargs))
+    return '{}?{}'.format(URL, urlencode(kwargs))
 
 
 def get_genres():
@@ -119,90 +140,88 @@ def list_genres():
     """
     # Set plugin category. It is displayed in some skins as the name
     # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, 'Public Domain Movies')
+    xbmcplugin.setPluginCategory(HANDLE, 'Public Domain Movies')
     # Set plugin content. It allows Kodi to select appropriate views
     # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'movies')
+    xbmcplugin.setContent(HANDLE, 'movies')
     # Get movie genres
     genres = get_genres()
     # Iterate through genres
-    for index, genre in enumerate(genres):
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=genre['genre'])
+    for index, genre_info in enumerate(genres):
+        # Create a list item with a text label.
+        list_item = xbmcgui.ListItem(label=genre_info['genre'])
+        # Set images for the list item.
+        list_item.setArt({'icon': genre_info['icon'], 'fanart': genre_info['fanart']})
         # Set additional info for the list item using its InfoTag.
-        # Here we use a category name for both properties for simplicity's sake.
         # InfoTag allows to set various information for an item.
         # For available properties and methods see the following link:
         # https://codedocs.xyz/xbmc/xbmc/classXBMCAddon_1_1xbmc_1_1InfoTagVideo.html
         # 'mediatype' is needed for a skin to display info for this ListItem correctly.
         info_tag = list_item.getVideoInfoTag()
         info_tag.setMediaType('video')
-        info_tag.setTitle(genre)
-        info_tag.setGenres([genre])
-        # Set icon for the list item.
-        # addAvailableArtwork() method can be used to add different types of images: poster, fanart, etc.
-        info_tag.addAvailableArtwork(genre['icon'], 'icon')
+        info_tag.setTitle(genre_info['genre'])
+        info_tag.setGenres([genre_info['genre']])
         # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=listing&genre_index=0
         url = get_url(action='listing', genre_index=index)
         # is_folder = True means that this item opens a sub-list of lower level items.
         is_folder = True
         # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
+        xbmcplugin.addDirectoryItem(HANDLE, url, list_item, is_folder)
     # Add sort methods for the virtual folder items
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+    xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
+    xbmcplugin.endOfDirectory(HANDLE)
 
 
 def list_videos(genre_index):
     """
     Create the list of playable videos in the Kodi interface.
 
-    :param genre_index: the index of genre in the list of videos
+    :param genre_index: the index of genre in the list of movie genres
     :type genre_index: int
     """
     genre_info = VIDEOS[genre_index]
     # Set plugin category. It is displayed in some skins as the name
     # of the current section.
-    xbmcplugin.setPluginCategory(_HANDLE, genre_info['genre'])
+    xbmcplugin.setPluginCategory(HANDLE, genre_info['genre'])
     # Set plugin content. It allows Kodi to select appropriate views
     # for this type of content.
-    xbmcplugin.setContent(_HANDLE, 'movies')
+    xbmcplugin.setContent(HANDLE, 'movies')
     # Get the list of videos in the category.
     videos = genre_info['movies']
     # Iterate through videos.
     for video in videos:
         # Create a list item with a text label
         list_item = xbmcgui.ListItem(label=video['title'])
-        # Set additional info for the list item.
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use only poster for simplicity's sake.
+        # In a real-life plugin you may need to set multiple image types.
+        list_item.setArt({'poster': video['poster']})
+        # Set additional info for the list item via InfoTag.
         # 'mediatype' is needed for skin to display info for this ListItem correctly.
         info_tag = list_item.getVideoInfoTag()
         info_tag.setMediaType('movie')
         info_tag.setTitle(video['title'])
-        info_tag.setGenres(genre_info['genre'])
+        info_tag.setGenres([genre_info['genre']])
         info_tag.setPlot(video['plot'])
         info_tag.setYear(video['year'])
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use only for simplicity's sake.
-        # In a real-life plugin you may need to set multiple image types.
-        info_tag.addAvailableArtwork(video['poster'], 'poster')
         # Set 'IsPlayable' property to 'true'.
         # This is mandatory for playable items!
         list_item.setProperty('IsPlayable', 'true')
         # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        # Example: plugin://plugin.video.example/?action=play&video=https%3A%2F%2Fia600702.us.archive.org%2F3%2Fitems%2Firon_mask%2Firon_mask_512kb.mp4
         url = get_url(action='play', video=video['url'])
         # Add the list item to a virtual Kodi folder.
         # is_folder = False means that this item won't open any sub-list.
         is_folder = False
         # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_HANDLE, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+        xbmcplugin.addDirectoryItem(HANDLE, url, list_item, is_folder)
+    # Add sort methods for the virtual folder items
+    xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_VIDEO_YEAR)
     # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_HANDLE)
+    xbmcplugin.endOfDirectory(HANDLE)
 
 
 def play_video(path):
@@ -213,10 +232,12 @@ def play_video(path):
     :type path: str
     """
     # Create a playable item with a path to play.
+    # offscreen=True means that the list item is not meant for displaying,
+    # only to pass info to the Kodi player
     play_item = xbmcgui.ListItem(offscreen=True)
     play_item.setPath(path)
     # Pass the item to the Kodi player.
-    xbmcplugin.setResolvedUrl(_HANDLE, True, listitem=play_item)
+    xbmcplugin.setResolvedUrl(HANDLE, True, listitem=play_item)
 
 
 def router(paramstring):
@@ -231,22 +252,21 @@ def router(paramstring):
     # {<parameter>: <value>} elements
     params = dict(parse_qsl(paramstring))
     # Check the parameters passed to the plugin
-    if params:
-        if params['action'] == 'listing':
-            # Display the list of videos in a provided category.
-            list_videos(int(params['genre_index']))
-        elif params['action'] == 'play':
-            # Play a video from a provided URL.
-            play_video(params['video'])
-        else:
-            # If the provided paramstring does not contain a supported action
-            # we raise an exception. This helps to catch coding errors,
-            # e.g. typos in action names.
-            raise ValueError('Invalid paramstring: {}!'.format(paramstring))
-    else:
+    if not params:
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
         list_genres()
+    elif params['action'] == 'listing':
+        # Display the list of videos in a provided category.
+        list_videos(int(params['genre_index']))
+    elif params['action'] == 'play':
+        # Play a video from a provided URL.
+        play_video(params['video'])
+    else:
+        # If the provided paramstring does not contain a supported action
+        # we raise an exception. This helps to catch coding errors,
+        # e.g. typos in action names.
+        raise ValueError(f'Invalid paramstring: {paramstring}!')
 
 
 if __name__ == '__main__':
